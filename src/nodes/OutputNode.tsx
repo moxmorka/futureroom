451e4e8bdfb4eb6e2dda96e6adff5b51f6b28044
moxmorka <132207337+moxmorka@users.noreply.github.com>
2026-03-02 19:55:01 +0000

    Create OutputNode.tsx

---
+++ src/nodes/OutputNode.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Handle, Position } from "reactflow";
+
+export function OutputNode() {
+  return (
+    <div className="node" style={{ width: 220 }}>
+      <div className="nodeHeader">
+        <div>
+          <div className="nodeTitle">Output</div>
+          <div className="nodeSub">speakers</div>
+        </div>
+        <div className="badge">DEST</div>
+      </div>
+      <div className="nodeBody">
+        <div className="hint">Connect audio here.</div>
+        <Handle className="handle-audio" type="target" position={Position.Left} id="audioIn" style={{ top: 28 }} />
+      </div>
+    </div>
+  );
+}
