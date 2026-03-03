748f5289c2960efb231cd20c92e00b16cee54b19
moxmorka <132207337+moxmorka@users.noreply.github.com>
2026-03-03 15:32:34 +0000

    Update Slider.tsx

--- src/ui/Slider.tsx
+++ src/ui/Slider.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export function Slider({
-  value, min, max, step, onChange,
+  value,
+  min,
+  max,
+  step,
+  onChange,
 }: {
   value: number;
   min: number;
@@ -11,12 +15,14 @@ export function Slider({
 }) {
   return (
     <input
-      className="input"
+      className="input nodrag"
       type="range"
       min={min}
       max={max}
       step={step ?? 1}
       value={value}
+      onPointerDown={(e) => e.stopPropagation()}
+      onPointerMove={(e) => e.stopPropagation()}
       onChange={(e) => onChange(Number(e.target.value))}
     />
   );
